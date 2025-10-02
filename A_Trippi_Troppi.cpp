56142d2a315567275bc7074f39ec366755266cca
Ahmed <Ahmeddiab@gmail.com>
2025-10-02 21:47:21 +0300

    Added solutions A_Trippi_Troppi.cpp

---
+++ A_Trippi_Troppi.cpp
@@ -0,0 +1,10 @@
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#include <bits\stdc++.h>
+using namespace std;
+int main(){
+    int t;cin>>t;while(t--){
+        string s,s1,s2;
+        cin>>s>>s1>>s2;
+        cout<<s[0]<<s1[0]<<s2[0]<<endl;
+    }
+}
